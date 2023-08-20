c2dcb9fbbd842c88514ea41260ae4f0ff0e071fc
Alex Polotsk <apolotsk@gmail.com>
2023-08-20 19:40:40 +0200

    Merge file Time.hpp import Stopwatch.hpp

--- MeasureSpeed/Stopwatch.hpp
+++ MeasureSpeed/Stopwatch.hpp
@@ -1,8 +1,15 @@
 #pragma once
-#include "Time.hpp"
+#include <chrono> // For `std::chrono`.
+
+struct SteadyClockTime {
+  std::chrono::steady_clock::time_point time_point;
+  SteadyClockTime() { time_point = std::chrono::steady_clock::now(); }
+  inline operator std::chrono::steady_clock::time_point() { return time_point; }
+  double Timestamp() const { return std::chrono::duration<double>(time_point.time_since_epoch()).count(); }
+};
 
 class Stopwatch {
-  typedef MonotonicTime TimeStruct;
+  typedef SteadyClockTime TimeStruct;
   TimeStruct time;
 public:
   /* Returns seconds since the last Split() or Stopwatch(), and resets timer. */
