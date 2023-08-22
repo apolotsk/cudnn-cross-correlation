14f82a27dce76356d52b8296c569dc526d141855
Alex Polotsk <apolotsk@gmail.com>
2023-08-22 13:30:14 +0200

    [cudart] Change function order

--- cudart/cudart.hpp
+++ cudart/cudart.hpp
@@ -5,12 +5,12 @@
 
 namespace cudart {
 
+#define cuda_assert(expr) _cuda_assert(expr, __FILE__, __LINE__, #expr);
 void _cuda_assert(cudaError_t error, const char* call_file, unsigned int call_line, const char* expression) {
   if (error==cudaSuccess) return;
   printf("Assertion in %s:%d %s. %s\n", call_file, call_line, expression, cudaGetErrorString(error));
   throw std::runtime_error(cudaGetErrorString(error));
 }
-#define cuda_assert(expr) _cuda_assert(expr, __FILE__, __LINE__, #expr);
 
 class DeviceData {
   size_t size;
