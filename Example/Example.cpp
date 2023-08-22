f9441f4715e15ca748fa58db611d3595b952d0c9
Alex Polotsk <apolotsk@gmail.com>
2023-08-22 16:59:45 +0200

    In the examples, print more

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -15,14 +15,23 @@ void save_image(const void* data, int height, int width, const char* filepath) {
   cv::imwrite(filepath, image);
 }
 
+#include <stdio.h> // For printf.
 #include <string.h> // For malloc, free.
 #include <CrossCorrelation.hpp>
 int main() {
   cv::Mat image = load_image("input.png");
+  printf("Load an image from input.png.\n");
+
   Tensor<float> input;
   input.Create(1, image.channels(), image.rows, image.cols, image.ptr(), NHWC);
+  printf("Create the input tensor.\n");
+  printf("Copy the image into the input tensor.\n");
+  printf("The input tensor shape is [%d, %d, %d, %d].\n", input.batch_size, input.height, input.width, input.depth);
+
   Filter<float> filter;
   filter.Create(1, input.depth, 3, 3, NULL);
+  printf("Create the filter.\n");
+  printf("The filter shape is [%d, %d, %d, %d].\n", filter.output_depth, filter.input_depth, filter.height, filter.width);
   auto filter_data = []()->const void* {
     static float data[1][3][3][3] = {{
       {
@@ -43,18 +52,25 @@ int main() {
     }};
     return data;
   };
+  printf("Create edge detection kernel data.\n");
   filter.CopyTo(filter_data());
+  printf("Copy the kernel data into the filter.\n");
+
   Tensor<float> output;
   output.Create(input.batch_size, filter.output_depth, input.height-filter.height+1, input.width-filter.width+1, NULL, NHWC);
+  printf("Create the output tensor.\n");
+  printf("The output tensor shape [%d, %d, %d, %d].\n", output.batch_size, output.height, output.width, output.depth);
 
   CrossCorrelation<float> cross_correlation;
   cross_correlation.Create();
   cross_correlation.Configure(input, filter, output);
   cross_correlation.Run(input, filter, output);
+  printf("Cross-correlate the input tensor and the filter.\n");
 
   void* output_data = malloc(output.size);
   output.CopyFrom(output_data);
   save_image(output_data, output.height, output.width, "output.png");
+  printf("Save the output tensor as an image to output.png.\n");
   free(output_data);
 
   cross_correlation.Destroy();
--- MeasureSpeed/MeasureSpeed.cpp
+++ MeasureSpeed/MeasureSpeed.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h> // For printf.
 #include <string.h> // For malloc, free.
 #include <CrossCorrelation.hpp>
 #include "Stopwatch.hpp"
@@ -11,6 +12,7 @@ int main() {
   rand<type>(input_data, input.size/sizeof(type));
   input.CopyTo(input_data);
   free(input_data);
+  printf("Create input tensor with shape [%d, %d, %d, %d] and random data.\n", input.batch_size, input.depth, input.height, input.width);
 
   Filter<type> filter;
   filter.Create(512, input.depth, 16, 16, NULL, NCHW);
@@ -18,6 +20,7 @@ int main() {
   rand<type>(filter_data, filter.size/sizeof(type));
   filter.CopyTo(filter_data);
   free(filter_data);
+  printf("Create filter with shape [%d, %d, %d, %d] and random data.\n", filter.output_depth, filter.input_depth, filter.height, filter.width);
 
   Tensor<type> output;
   output.Create(input.batch_size, filter.output_depth, input.height-filter.height+1, input.width-filter.width+1, NULL, NCHW);
@@ -32,7 +35,7 @@ int main() {
   for (int i = 0; i<count; ++i) {
     cross_correlation.Run(input, filter, output);
   }
-  printf("time = %.1f ms\n", stopwatch.Time()/count*1e3); // Jetson Nano: 27.2 ms.
+  printf("Cross-correlation takes %.1f ms.\n", stopwatch.Time()/count*1e3);
 
   cross_correlation.Destroy();
   output.Destroy();
