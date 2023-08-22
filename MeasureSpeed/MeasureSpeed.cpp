aa49fc19511f55a055e3f8b7b4a30e1545466a08
Alex Polotsk <apolotsk@gmail.com>
2023-08-22 17:20:13 +0200

    [MeasureSpeed] Modularize

--- MeasureSpeed/MeasureSpeed.cpp
+++ MeasureSpeed/MeasureSpeed.cpp
@@ -21,36 +21,45 @@ double timestamp() {
 int main() {
   typedef float type;
   Tensor<type> input;
-  input.Create(1, 1, 128, 128, NULL, NCHW);
-  void* input_data = malloc(input.size);
-  rand<type>(input_data, input.size/sizeof(type));
-  input.CopyFrom(input_data);
-  free(input_data);
-  printf("Create input tensor with shape [%d, %d, %d, %d] and random data.\n", input.batch_size, input.depth, input.height, input.width);
+  {
+    input.Create(1, 1, 128, 128, NULL, NCHW);
+    void* input_data = malloc(input.Size());
+    rand<type>(input_data, input.Size()/sizeof(type));
+    input.CopyFrom(input_data);
+    free(input_data);
+    printf("Create the input tensor with shape [%d, %d, %d, %d] and random data.\n", input.batch_size, input.depth, input.height, input.width);
+  }
 
   Filter<type> filter;
-  filter.Create(512, input.depth, 16, 16, NULL, NCHW);
-  void* filter_data = malloc(filter.size);
-  rand<type>(filter_data, filter.size/sizeof(type));
-  filter.CopyFrom(filter_data);
-  free(filter_data);
-  printf("Create filter with shape [%d, %d, %d, %d] and random data.\n", filter.output_depth, filter.input_depth, filter.height, filter.width);
+  {
+    filter.Create(512, input.depth, 16, 16, NULL, NCHW);
+    void* filter_data = malloc(filter.Size());
+    rand<type>(filter_data, filter.Size()/sizeof(type));
+    filter.CopyFrom(filter_data);
+    free(filter_data);
+    printf("Create the filter with shape [%d, %d, %d, %d] and random data.\n", filter.output_depth, filter.input_depth, filter.height, filter.width);
+  }
 
   Tensor<type> output;
-  output.Create(input.batch_size, filter.output_depth, input.height-filter.height+1, input.width-filter.width+1, NULL, NCHW);
+  {
+    output.Create(input.batch_size, filter.output_depth, input.height-filter.height+1, input.width-filter.width+1, NULL, NCHW);
+    printf("Create the output tensor with shape [%d, %d, %d, %d].\n", output.batch_size, output.depth, output.height, output.width);
+  }
 
   CrossCorrelation<float> cross_correlation;
-  cross_correlation.Create();
-  cross_correlation.Configure(input, filter, output);
-  cross_correlation.Run(input, filter, output);
-
-  double timestamp0 = timestamp();
-  const int count = 10;
-  for (int i = 0; i<count; ++i) {
+  {
+    cross_correlation.Create();
+    cross_correlation.Configure(input, filter, output);
     cross_correlation.Run(input, filter, output);
+
+    double timestamp0 = timestamp();
+    const int count = 10;
+    for (int i = 0; i<count; ++i) {
+      cross_correlation.Run(input, filter, output);
+    }
+    double timestamp1 = timestamp();
+    printf("Cross-correlation takes %.1f ms (on average over %d runs).\n", (timestamp1-timestamp0)/count*1e3, count);
   }
-  double timestamp1 = timestamp();
-  printf("Cross-correlation takes %.1f ms.\n", (timestamp1-timestamp0)/count*1e3);
 
   cross_correlation.Destroy();
   output.Destroy();
