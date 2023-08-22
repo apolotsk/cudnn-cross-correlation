feaf0cadfb2bd8e341d4c6d8e0e024cbce5d5482
Alex Polotsk <apolotsk@gmail.com>
2023-08-22 15:55:55 +0200

    In function `cuda_assert`, throw and exception instead of exiting

--- cuda/cuda.hpp
+++ cuda/cuda.hpp
@@ -1,11 +1,12 @@
 #pragma once
-
 #include <stdio.h> // For `printf()`.
+#include <stdexcept> // For std::runtime_error().
 #include <stdlib.h> // For `exit()`.
 #include <cuda_runtime.h>
-void _cuda_assert(cudaError_t error, int line) {
+
+void _cuda_assert(cudaError_t error, const char* call_file, unsigned int call_line, const char* expression) {
   if (error==cudaSuccess) return;
-  printf("Error at line %d: %s.\n", line, cudaGetErrorString(error));
-  exit(1);
+  printf("Assertion in %s:%d %s. %s\n", call_file, call_line, expression, cudaGetErrorString(error));
+  throw std::runtime_error(cudaGetErrorString(error));
 }
-#define cuda_assert(status) _cuda_assert(status, __LINE__);
+#define cuda_assert(expr) _cuda_assert(expr, __FILE__, __LINE__, #expr);
